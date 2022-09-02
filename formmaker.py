fef10fa0dd3ad467fd5617b55682d9f1edfb02bd
MOTHMAN-404 <shawnmcdermott17@gmail.com>
2022-09-02 00:02:09 -0400

    Changed libary and made it look a bit better

--- formmaker.py
+++ formmaker.py
@@ -1,23 +1,56 @@
 import docx
+from docx.shared import Cm, Inches
+
+# paragraph_format.left_indent = Inches(0.5)
+# paragraph_format.first_line_indent
+#Set font
+# font = run.font
+# font.size = Pt(16)
 
 not_finished = True
+document_number = 1
 while(not_finished):
 
     # Create instance of word document
     doc = docx.Document()
 
     # Write the first section of the document
-    doc_para = doc.add_paragraph('''Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.''')
+    doc_paragraph1 = doc.add_paragraph('''Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.''')
+    par1 = doc_paragraph1.add_run()
+    par1.add_break()
+
 
 
     # Create the table 
     table = doc.add_table(rows=5, cols=4)
     table.style = "Table Grid"
-    row1 = table.rows[0].cells
-    row1[0].text = "Bitlocker";
-    row1[1].text = "Serial Number";
-    row1[2].text = "Employee Name / Job Title";
-    row1[3].text = "Description";
+    # table.allow_autofit = False
+    table.columns[0].width = Inches(1.0)
+    table.rows[0].cells[0].width = Inches(1.0)
+
+
+    #Fill in First Row
+    i = 0
+    for cell in table.rows[0].cells: 
+        paragraph = cell.paragraphs[0]
+        if i == 0:
+            run = paragraph.add_run("Bitlocker")
+        elif i == 1:
+            run = paragraph.add_run("Serial Number")
+        elif i == 2:
+            run = paragraph.add_run("Employee Name / Job Title")
+        elif i == 3:    
+            run = paragraph.add_run("Description")
+        run.bold = True     
+        i = i + 1
+
+    #for i in range(4):
+        #for cell in table.rows[i].cells:
+            #if i == 2:
+               # cell.width = Inches(1)
+               # cell.height = Inches (0.25)
+            #else:
+                #cell.width = Inches(0.5)
 
     # Get the inputs for the table
     for i in range(4):
@@ -26,6 +59,8 @@ while(not_finished):
         employee_name = input("Enter the employee name: ")
         job_title = input("Enter the job_title: ")
         description = input("Enter the description: ")
+
+        print("-----------------------------------")
 
         name_desc = employee_name + "/" + job_title
         row = table.rows[i+1].cells
@@ -37,13 +72,18 @@ while(not_finished):
 
 
     # Write the third section of the document
-    doc_para = doc.add_paragraph("ultrices neque. Vulputate eu scelerisque felis imperdiet proin. Tincidunt augue interdum velit euismod in pellentesque. Porttitor lacus luctus accumsan tortor posuer")
-    doc_para = doc.add_paragraph("Now Sign here __________________")
-
+    doc_paragraph2 = doc.add_paragraph("")
+    par2 = doc_paragraph2.add_run()
+    par2.add_break()
+    doc.add_paragraph("ultrices neque. Vulputate eu scelerisque felis imperdiet proin. Tincidunt augue interdum velit euismod in pellentesque. ")
+    doc.add_paragraph("A erat nam at lectus urna duis. Tortor condimentum lacinia quis vel eros donec ac odio. Maecenas pharetra convallis posuere morbi. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing.")
+    doc.add_paragraph("Sign here ________________________________________________________")
+    doc.add_paragraph("Sign here ________________________________________________________")
 
 
     # Save
-    doc.save("document.docx")
+    save_name = "document" + str(document_number) + ".docx"
+    doc.save(save_name)
 
     # Check if the user would like to continue
     not_finished = input("Would you like to continue (y/n): ")
